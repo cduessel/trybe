abb40f0d353f9c910d09231d7de7040350406b9e
Christian Duessel <duessel@gmail.com>
2020-05-12 18:13:12 -0300

    exercicios 1-4

---
+++ 4.JavaScript/3maismaior.js
@@ -0,0 +1,11 @@
+//3- Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+if ( a > b && a > c ){ res= a }
+else if ( b > a && b > c){ res = b}
+else { res = c }
+
+console.log(res)
---
+++ 4.JavaScript/4pnz.js
@@ -0,0 +1,8 @@
+// 4- Faça um programa que, dado um valor definido numa variável, retorne “positive” se esse valor for positivo, “negative” se for negativo e “zero” caso contrário.
+let a = -3;
+
+if (a > 0 ){ res = "positive" }
+else if ( a < 0 ){ res = "negative" }
+else { res = "zero" }
+
+console.log(res)

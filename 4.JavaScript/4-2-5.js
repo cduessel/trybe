d3aec015bb2c2c694d69f1ebd42771c4c24d30c0
Christian Duessel <duessel@gmail.com>
2020-05-13 19:02:12 -0300

    4.2 exercises part2

--- 4.JavaScript/4-2-5.js
+++ 4.JavaScript/4-2-5.js
@@ -5,10 +5,9 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maior = 0
 
 for ( let i=0; i<numbers.length; i+= 1){
-    for (let x = numbers.length; x>0 ; x-= 1){
-    if(numbers[i] > numbers[x]){maior = numbers[i]}
+        if(numbers[i] > maior){maior = numbers[i]}
      
-    }
+    
 }
 
 console.log(maior)

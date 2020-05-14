f75c33f447c9e865fb92f805f59d59a59024885d
Christian Duessel <duessel@gmail.com>
2020-05-14 13:47:18 -0300

    4.2 exercises bonus

--- 4.JavaScript/4-2-2.js
+++ 4.JavaScript/4-2-2.js
@@ -6,3 +6,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 console.log(
     numbers.reduce((a, b) => a + b, 0)
   )
+
+  let soma = 0
+  for(let i = 0; i<numbers.length; i+=1){
+      soma+= numbers[i]
+  }
+  console.log(soma)
+  

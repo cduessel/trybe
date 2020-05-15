15437d407e18587e18450cd7e6d61ce1fe46229d
Christian Duessel <duessel@gmail.com>
2020-05-15 20:07:35 -0300

    4.3 & 4.4 exercises

---
+++ 4.JavaScript/4-4-4.js
@@ -0,0 +1,17 @@
+
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+  //Faça um for/in que mostre todas as chaves do objeto.
+
+  info.recorrente="sim";
+  
+for (let i in info)
+{
+    console.log(i)
+}
+  
---
+++ 4.JavaScript/4-4-8.js
@@ -0,0 +1,18 @@
+
+//  Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+
+let array = [2, 3, 6, 7, 10, 1];
+
+function maiorIndice (){
+    let novapalavra = "";
+
+for (let i = (palavra.length-1); i>-1; i--){
+    novapalavra = novapalavra + palavra[i];
+    };
+    console.log(novapalavra);
+
+if (palavra === novapalavra){return true}    
+else {return false};
+}
+console.log(pali())

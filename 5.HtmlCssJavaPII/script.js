ac6b1a794b6dff07a447703f51caccd5f777d2b3
Christian Duessel <duessel@gmail.com>
2020-05-29 20:01:42 -0300

    subindo versão 6.2

--- 5.HtmlCssJavaPII/script.js
+++ 5.HtmlCssJavaPII/script.js
@@ -1,3 +1,27 @@
-const estados = ['','Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo',
+// window.onload = alert ("scripto!")
+
+window.JustValidate('.js-form');
+
+const estados = ['selecione aqui o seu','Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo',
 'Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco',
 'Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins']
+
+window.onload = function () {
+    for (let i = 0; i < estados.length; i += 1) {
+        let obj = document.createElement('option');
+        obj.innerHTML = estados[i];
+        obj.setAttribute('value', estados[i]);
+        document.getElementById('est').appendChild(obj);
+    };
+};
+
+let dados = document.getElementsByTagName('input');
+
+function resumodetd(){
+    for (let j = 0; j < dados.length; j += 1){
+        let infos = infos && dados[j].value; 
+    };
+
+    document.getElementById('resumo').innerHTML = infos };
+    
+document.getElementById('btn').addEventListener('click', resumodetd)

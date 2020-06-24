33bf248c06c3588040b38f3796134d32ef1bfcab
Christian Duessel <duessel@gmail.com>
2020-06-23 22:09:24 -0300

    JEST class

---
+++ 2. Front End - HTML, CSS, JS/8. JavaScript - JEST/testes.test.js
@@ -0,0 +1,44 @@
+const calcs = require('./calcs')
+const array = require('./array')
+const exercicios = require('./exercicios')
+
+describe.skip('Testes arquivos calcs e array', () => {
+
+    test('soma 1 e 2 para dar 3', () => {
+       expect(calcs.sum(1, 2)).toEqual(3)
+    })
+  
+    test('divide 10 por 3 para dar 3.33...', () => {
+       expect(calcs.division(10, 3)).toBeCloseTo(3.33)
+    })
+  
+    test('retorna apenas os pares no array', () => {
+       let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+       let array2 = [2, 4, 6, 8, 10]
+       expect(array.retornePares(array1)).toEqual(array2)
+    })
+
+    test('to contain', () => {
+        let arraycont = ['item1', 'item2', 'item3']
+        expect(array.addItem4(arraycont)).toContain('item4')
+      })
+  
+    test('3 mais 3', () => {
+    let valor = 3 + 3
+    expect(valor).toBeGreaterThan(5) ///maior que 5
+    expect(valor).toBeGreaterThanOrEqual(6) ///maior ou igual a 6
+    expect(valor).toBeLessThan(8) ///menor que 8
+    expect(valor).toBeLessThanOrEqual(7) ///menor ou igual a 7
+    expect(valor).not.toEqual(5) ///não igual a 5
+    })
+
+  })   
+  
+  describe('testes dos exercicios', () => {
+   
+    test('exercicio1: soma', () => {
+        expect(exercicios.sum(4, 5)).toEqual(9)
+        expect(exercicios.sum(0, 0)).toEqual(0)
+        expect(exercicios.sum(4, "5")).toThrow(“parameters must be numbers”)
+    })
+  })
